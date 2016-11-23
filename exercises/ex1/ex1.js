9aba30a706a3fa298d83bf297b8f721b89850483
Brooks Patton <brooks@brookzerker.com>
2016-11-22 21:53:19 -0700

    solved ex1 in my own way

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -21,10 +21,44 @@ function output(text) {
 // The old-n-busted callback way
 
 function getFile(file) {
+  handleResponse.register(file);
+
 	fakeAjax(file,function(text){
 		// what do we do here?
+    handleResponse.resolve(file, text);
 	});
 }
+
+var handleResponse = {
+  store: [],
+
+  register: function(file) {
+    this.store.push({
+      file: file,
+      resolved: false
+    });
+  },
+
+  resolve: function(file, text) {
+    var obj;
+
+    this.store.forEach(function(item) {
+      if(item.file === file) {
+        obj = item;
+      }
+    });
+
+    obj.resolved = true;
+    obj.text = text;
+
+    var current;
+
+    while(this.store.length > 0 && this.store[0].resolved) {
+      current = this.store.shift();
+      output(current.text);
+    }
+  }
+};
 
 // request all files at once in "parallel"
 getFile("file1");
